f1c6e0e21142d519998a018fb694b6fd87098088
Lemixus <lemix.kotova@gmail.com>
2025-05-17 17:59:13 +0300

    fastest car feature

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,49 @@
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        RaceClass race = new RaceClass();
+
+        for (int i = 1; i <= 3; i++) {
+            int speed = 0;
+            String name = "";
+
+            while (true) {
+                System.out.println("Введите название машины " + i + ":");
+                name = scanner.nextLine().trim();
+
+                if (!name.isEmpty())
+                    break;
+                else
+                    System.out.println("Введите непустое название машины");
+
+            }
+
+            while (true) {
+                System.out.println("Введите скорость машины " + i + ":");
+                if (scanner.hasNextInt()) {
+                    speed = scanner.nextInt();
+                    if (speed > 0 && speed <= 250) {
+                        scanner.nextLine();
+                        break;
+                    } else {
+                        System.out.println("Введите скорость от 1 до 250");
+                    }
+
+                } else {
+                    System.out.println("Введите целое число");
+                    scanner.next();
+                }
+            }
+
+            CarClass car = new CarClass(name, speed);
+            race.calculateRaceLeader(car);
+        }
+
+        System.out.println("Самая быстрая машина: " + race.leader);
+        scanner.close();
     }
 }
